a5bb7b89df4bcdef7e4a5030942e47a3f6be5766
anomepani <arvindmepani@gmail.com>
2017-02-20 00:29:03 +0530

    Initial commit for meteor boiler plate code

---
+++ imports/ui/pages/hello.js
@@ -0,0 +1,166 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Promise } from 'meteor/promise';
+
+import './hello.html';
+
+Template.helloOld.onCreated(function helloOnCreated() {
+    // counter starts at 0
+    console.log('template created');
+    this.counter = new ReactiveVar(0);
+    //Create reactive var for array
+    this.list = new ReactiveVar([]);
+    this.listAsync = new ReactiveVar([]);
+    this.listPromise = new ReactiveVar([]);
+    console.log('before calling getHobbits');
+    // Meteor.call('getHobbits', (error, result) => {
+    //     console.log('getHobbits Response received');
+    //     console.log(error);
+    //     //  console.log(result);
+    //     this.list.set(result);
+    // });
+    // Meteor.call('getHobbitsWithFiber', (error, result) => {
+    //     console.log('getHobbits Response received');
+    //     console.log(error);
+    //     //  console.log(result);
+    //     this.list.set(result);
+    // });
+
+    console.log('before calling getHobbitsAsync');
+    Meteor.call('getHobbitsFinal', (error, result) => {
+        console.log('getHobbitsAsync Response received');
+        // console.log(error);
+        console.log(result);
+        this.listAsync.set(result);
+    });
+    console.log('before calling getHobbitsPromise');
+
+    Meteor.call('getHobbitsPromise', (error, result) => {
+        console.log('getHobbitsPromise Response received');
+        //console.log(error);
+        console.log(result);
+        this.listPromise.set(result);
+    });
+    Meteor.call('getHobbitsError', (error, result) => {
+        console.log('getHobbitsError Response received');
+        console.log(error);
+        //  console.log(result);
+
+    });
+
+
+});
+
+Template.helloOld.helpers({
+    counter() {
+        return Template.instance().counter.get();
+    },
+    hobbits() {
+        return Template.instance().list.get();
+    },
+    hobbitsZsync() {
+        return Template.instance().listAsync.get();
+    },
+    hobbitsPromise() {
+        return Template.instance().listPromise.get();
+    }
+});
+
+Template.promiseHelloWorld.events({
+    'click button' (event, instance) {
+        // increment the counter when button is clicked
+        instance.counter.set(instance.counter.get() + 1);
+    },
+});
+
+Template.promiseHelloWorld.onCreated(async function helloOnCreated() {
+    this.list = new ReactiveVar([]);
+    //OLD WAY TO USE async and await in client side meteor
+    this.list.set(await callWithPromise('getHobbits'));
+});
+
+Template.promiseHelloWorld.helpers({
+    hobbits() {
+        return Template.instance().list.get();
+    },
+});
+
+/**
+ * Created method to understand how to wrap meteor call in Promise syntax
+ * You can use 'https://atmospherejs.com/deanius/promise' package
+ */
+export const callWithPromise = (method) => {
+    return new Promise((resolve, reject) => {
+        Meteor.call(method, (error, result) => {
+            if (error) reject(error);
+            resolve(result);
+        });
+    });
+}
+
+/**
+ * Custom settimeout wrapper within Promise
+ */
+const sleep = (mS) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, mS);
+    });
+}
+
+
+
+Template.hello.onCreated(function helloOnCreated() {
+    this.list = new ReactiveVar([]);
+    this.latestError = new ReactiveVar();
+    ////OLD CODE START
+    // Meteor.call('assignRing', 1, (error, result) => {
+    //     if (error) {
+    //         this.latestError.set(error.message);
+    //     } else {
+    //         Meteor.call('getHobbits', (error, hobbits) => {
+    //             if (error) {
+    //                 this.latestError.set(error.message);
+    //             } else {
+    //                 this.list.set(hobbits);
+    //                 Meteor.setTimeout(() => {
+    //                     Meteor.call('assignRing', 2, (error, result) => {
+    //                         if (error) {
+    //                             this.latestError.set(error.message);
+    //                         } else {
+    //                             Meteor.call('getHobbits', (error, hobbits) => {
+    //                                 if (error) {
+    //                                     this.latestError.set(error.message);
+    //                                 } else {
+    //                                     this.list.set(hobbits);
+    //                                 }
+    //                             });
+    //                         }
+    //                     });
+    //                 }, 1000);
+    //             }
+    //         });
+    //     }
+    // });
+
+    ////OLD CODE END
+
+    await Meteor.callPromise('assignRing', 1);
+    this.list.set(await Meteor.callPromise('getHobbits'));
+    await sleep(1000);
+    await Meteor.callPromise('assignRing', 2);
+    this.list.set(await Meteor.callPromise('getHobbits'));
+} catch (error) {
+    this.latestError.set(error.message);
+}
+});
+
+Template.hello.helpers({
+    hobbits() {
+        return Template.instance().list.get();
+    },
+    errorMessage() {
+        return Template.instance().latestError.get();
+    },
+});
---
+++ server/main.js
@@ -0,0 +1,172 @@
+import { Meteor } from 'meteor/meteor';
+import mysql from 'promise-mysql';
+
+import { Mongo } from 'meteor/mongo';
+export const Hobbitses = new Mongo.Collection('Hobbitses');
+
+Meteor.startup(() => {
+
+    mysql.createConnection({
+        host: 'localhost',
+        user: 'sauron',
+        password: 'theonetruering',
+        database: 'mordor'
+    }).then(conn => {
+        connection = conn;
+        //console.log(connection);
+        return connection.query('select `name` from hobbits');
+    }).then(result => {
+        console.log(result);
+    });
+});
+
+Meteor.methods({
+    getHobbits1() {
+        let connection;
+
+        mysql.createConnection({
+            host: 'localhost',
+            user: 'sauron',
+            password: 'theonetruering',
+            database: 'mordor'
+        }).then(conn => {
+            connection = conn;
+            return connection.query('select `name` from hobbits');
+        }).then(result => {
+            return result; // will NOT work!
+        });
+    },
+    getHobbits2() {
+        let connection;
+        let rows;
+
+        mysql.createConnection({
+            host: 'localhost',
+            user: 'sauron',
+            password: 'theonetruering',
+            database: 'mordor'
+        }).then(conn => {
+            connection = conn;
+            return connection.query('select `name` from hobbits');
+        }).then(result => {
+            rows = result;
+        });
+        return rows; // will NOT work!
+    },
+
+    async getHobbitsAsync() {
+        console.log('getHobbitsAsync called');
+        const connection = await mysql.createConnection({
+            host: 'localhost',
+            user: 'sauron',
+            password: 'theonetruering',
+            database: 'mordor'
+        });
+        const result = await connection.query('select `name` from hobbits limit 100');
+        console.log('getHobbitsAsync executed&&&');
+        return result;
+    },
+    getHobbitsPromise() {
+        const connection = Promise.await(mysql.createConnection({
+            host: 'localhost',
+            user: 'sauron',
+            password: 'theonetruering',
+            database: 'mordor'
+        }));
+        const rows = Promise.await(connection.query('select `name` from hobbits'));
+        return rows;
+    },
+    async getHobbitsError() {
+        let connection;
+        try {
+            connection = await mysql.createConnection({
+                host: 'localhost',
+                user: 'sauron',
+                password: 'theonetruering',
+                database: 'mordor'
+            });
+            const result = await connection.query('select `name` from hobbits');
+            return result;
+        } catch (error) {
+            console.log(error);
+            throw new Meteor.Error('oops', 'something bad happened');
+        } finally {
+            if (connection && connection.end) await connection.end(); // Let's be nice and close the connection
+        }
+    },
+    getHobbitsWithFiber() {
+        let connection;
+
+        const promisedResult = mysql.createConnection({
+            host: 'localhost',
+            user: 'sauron',
+            password: 'theonetruering',
+            database: 'mordor'
+        }).then(conn => {
+            connection = conn;
+            return connection.query('select `name` from hobbits');
+        }).then(result => {
+            result.forEach(row => {
+                //OLD Code throwing exception:Meteor code must always run within a Fiber.
+                // Try wrapping callbacks that you pass to non-Meteor libraries with Meteor.bindEnvironment.
+                //Hobbitses.insert({ name: row.name });
+                // New Code which is aware about Fiber
+                //Below rawCollection API and minimongodb _id column have diffrent data type
+                //Hobbitses.rawCollection().insert({ name: row.name });
+                //Set your own random id
+                Hobbitses.rawCollection().insert({ _id: Random.id(), name: row.name });
+
+            });
+            return result;
+        });
+        return promisedResult;
+    },
+    async getHobbitsFinal() {
+        const connection = await mysql.createConnection({
+            host: 'localhost',
+            user: 'sauron',
+            password: 'theonetruering',
+            database: 'mordor'
+        });
+        const result = await connection.query('select `name` from hobbits');
+        result.forEach(row => {
+            Hobbitses.insert({ name: row.name });
+        });
+        await connection.end();
+        return result;
+    },
+    async getHobbits() {
+        let connection;
+        try {
+            connection = await mysql.createConnection(connectionParameters);
+            const result = await connection.query(
+                `select
+          h.name,
+          i.name as item
+        from hobbits as h
+        left outer join items as i on i.owner = h.id
+        order by h.id`);
+            return result;
+        } catch (error) {
+            throw new Meteor.Error('getHobbits', 'something bad happened');
+        } finally {
+            if (connection && connection.end) await connection.end(); // Let's be nice and close the connection
+        }
+    },
+    async assignRing(id) {
+        let connection;
+        try {
+            connection = await mysql.createConnection(connectionParameters);
+            const result = await connection.query(
+                `update items
+          set owner = ${id}
+        where name = 'ring'`); // Really should have an index on items.name, but hey.
+            return result;
+        } catch (error) {
+            throw new Meteor.Error('assignRing', 'something bad happened');
+        } finally {
+            if (connection && connection.end) await connection.end(); // Let's be nice and close the connection
+        }
+    },
+
+});
